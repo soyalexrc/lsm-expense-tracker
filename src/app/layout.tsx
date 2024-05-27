74630c214f199ef5ac6f7fb9e64c58bb58555872
alexrodriguez <alexcarvajal2404@gmail.com>
2024-05-27 20:05:48 +0200

    :zap: shadcn/ui configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+      <head >
+          <title>LSM Expense Tracker</title>
+      </head>
+      <body
+          className={cn(
+              "min-h-screen bg-background font-sans antialiased",
+              fontSans.variable
+          )}
+      >
+      {children}
+      </body>
+      </html>
   );
 }
