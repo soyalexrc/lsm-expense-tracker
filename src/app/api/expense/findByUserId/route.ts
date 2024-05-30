52fc0d24acaae0dbf0a6f3cfee5893521f9669bb
alexrodriguez <alexcarvajal2404@gmail.com>
2024-05-30 12:02:35 -0500

    :zap:

--- src/app/api/expense/findByUserId/route.ts
+++ src/app/api/expense/findByUserId/route.ts
@@ -6,22 +6,22 @@ import {auth} from "@clerk/nextjs/server";
 
 export async function POST(req: NextRequest) {
     const { userId } = auth();
-    const {dateFrom, dateTo, categoryId, paymentMethod, title} = await req.json();
-    const query: any = { userId: userId };
-
-    // Add filters based on provided values
-    if (dateFrom && dateTo) {
-        query.date = { $gte: dateFrom, $lte: dateTo }; // Filter by date range
-    }
-    if (categoryId) {
-        query.category = categoryId; // Filter by category
-    }
-    if (paymentMethod) {
-        query.paymentMethod = paymentMethod; // Filter by payment method
-    }
-    if (title) {
-        query.title = { $regex: new RegExp(title, 'i') }; // Filter by title (case-insensitive)
-    }
+    // const {dateFrom, dateTo, categoryId, paymentMethod, title} = await req.json();
+    // const query: any = { userId: userId };
+    //
+    // // Add filters based on provided values
+    // if (dateFrom && dateTo) {
+    //     query.date = { $gte: dateFrom, $lte: dateTo }; // Filter by date range
+    // }
+    // if (categoryId) {
+    //     query.category = categoryId; // Filter by category
+    // }
+    // if (paymentMethod) {
+    //     query.paymentMethod = paymentMethod; // Filter by payment method
+    // }
+    // if (title) {
+    //     query.title = { $regex: new RegExp(title, 'i') }; // Filter by title (case-insensitive)
+    // }
     try {
         await connect()
         const data =  await Expense
